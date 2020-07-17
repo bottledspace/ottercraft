c255b8190812f38b099743cf8c7c4c27d0c17399
astronotter <ottersocks@outlook.com>
2020-07-17 11:36:35 -0400

    Tweaked run animation.

--- ottercraft-forge/src/main/java/ca/otterspace/ottercraft/OtterEntity.java
+++ ottercraft-forge/src/main/java/ca/otterspace/ottercraft/OtterEntity.java
@@ -11,16 +11,25 @@ public class OtterEntity extends WolfEntity {
     };
     public Animation animation;
     public int frame;
+    public int dframe;
     public OtterEntity(EntityType<? extends WolfEntity> type, World worldIn) {
         super(type, worldIn);
         this.animation = Animation.IDLE;
         this.frame = 0;
+        this.dframe = 1;
     }
     public void livingTick() {
         super.livingTick();
         double dx = Math.abs(this.getMotion().getX());
         double dz = Math.abs(this.getMotion().getZ());
-        this.animation = (dx*dx+dz*dz <= 0.01)? Animation.IDLE : Animation.RUNNING;
-        this.frame = (this.frame+1) % 20;
+
+        this.animation = (dx*dx+dz*dz <= 0.001)? Animation.IDLE : Animation.RUNNING;
+
+        this.frame = (this.frame + this.dframe) % 20;
+        if (this.animation == Animation.RUNNING) {
+            this.dframe = (this.dframe == 1) ? 2 : 1;
+        }
+        else
+            this.dframe = 1;
     }
 }
