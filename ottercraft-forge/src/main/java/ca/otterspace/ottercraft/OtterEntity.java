3a9bcded0d8b963a192a8dcf1a4238b15d625c82
astronotter <ottersocks@outlook.com>
2020-07-24 12:31:38 -0400

    Make giant otters rideable.

--- ottercraft-forge/src/main/java/ca/otterspace/ottercraft/OtterEntity.java
+++ ottercraft-forge/src/main/java/ca/otterspace/ottercraft/OtterEntity.java
@@ -1,15 +1,18 @@
 package ca.otterspace.ottercraft;
 
-import net.minecraft.entity.AgeableEntity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ILivingEntityData;
-import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.*;
 import net.minecraft.entity.passive.WolfEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
@@ -64,6 +67,67 @@ public class OtterEntity extends WolfEntity {
         }
         else
             this.dframe = 1;
+    }
+    public Entity getControllingPassenger() {
+        return this.getPassengers().isEmpty() ? null : this.getPassengers().get(0);
+    }
+    public boolean canBeSteered() { return true; }
+    public boolean processInteract(PlayerEntity player, Hand hand) {
+        ItemStack itemstack = player.getHeldItem(hand);
+
+        if (itemstack.getItem() == Items.NAME_TAG) {
+            itemstack.interactWithEntity(player, this, hand);
+            return true;
+        } else if (!this.isBeingRidden()) {
+            if (this.getOtterSubspecies() == 1 && !this.world.isRemote) {
+                player.startRiding(this);
+            }
+            return true;
+        } else
+            return false;
+    }
+    public void travel(Vec3d p_213352_1_) {
+        if (this.isAlive()) {
+            LivingEntity entity = (LivingEntity)getControllingPassenger();
+
+            if (this.isBeingRidden() && this.canBeSteered()) {
+                float strafe = entity.moveStrafing * 0.5F;
+                float forward = entity.moveForward;
+                this.rotationYaw = entity.rotationYaw;
+                this.prevRotationYaw = this.rotationYaw;
+                this.rotationPitch = entity.rotationPitch * 0.5F;
+                this.setRotation(this.rotationYaw, this.rotationPitch);
+                this.renderYawOffset = this.rotationYaw;
+                this.rotationYawHead = this.rotationYaw;
+                this.stepHeight = 1.0F;
+                this.jumpMovementFactor = this.getAIMoveSpeed() * 0.1F;
+
+                if (this.canPassengerSteer()) {
+                    float f = (float)this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getValue() * 0.225F;
+
+                    this.setAIMoveSpeed(f);
+                    super.travel(new Vec3d((double)strafe, p_213352_1_.y, (double)forward));
+                    this.newPosRotationIncrements = 0;
+                } else {
+                    this.setMotion(Vec3d.ZERO);
+                }
+
+                this.prevLimbSwingAmount = this.limbSwingAmount;
+                double d1 = this.getPosX() - this.prevPosX;
+                double d0 = this.getPosZ() - this.prevPosZ;
+                float f1 = MathHelper.sqrt(d1 * d1 + d0 * d0) * 4.0F;
+                if (f1 > 1.0F) {
+                    f1 = 1.0F;
+                }
+
+                this.limbSwingAmount += (f1 - this.limbSwingAmount) * 0.4F;
+                this.limbSwing += this.limbSwingAmount;
+            } else {
+                this.stepHeight = 0.5F;
+                this.jumpMovementFactor = 0.02F;
+                super.travel(p_213352_1_);
+            }
+        }
     }
     public static class OtterData extends AgeableEntity.AgeableData {
         public final int subspecies;
