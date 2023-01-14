c63d92720a2d82e7d1ca0dbac13e7eef01d345b0
bottledspace <andrew.pregent@gmail.com>
2023-01-14 18:48:51 -0500

    Rename OttercraftCommon -> Otter (No need for Common distinction now)

--- ottercraft-forge-1.19.3/src/main/java/ca/otterspace/ottercraft/Ottercraft.java
+++ ottercraft-forge-1.19.3/src/main/java/ca/otterspace/ottercraft/Ottercraft.java
@@ -1,6 +1,5 @@
 package ca.otterspace.ottercraft;
 
-import com.google.common.eventbus.Subscribe;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
@@ -27,8 +26,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-@Mod(OttercraftCommon.MODID)
-public class OttercraftCommon {
+@Mod(Ottercraft.MODID)
+public class Ottercraft {
     public static final String MODID = "ottercraft";
 
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MODID);
@@ -45,10 +44,10 @@ public class OttercraftCommon {
                     .sized(1.0f, 1.0f)
                     .build(new ResourceLocation(MODID, "otter").toString()));
     public static final RegistryObject<SpawnEggItem> OTTER_SPAWN_EGG = ITEMS.register("spawn_egg_otter",
-            () -> new ForgeSpawnEggItem(OTTER, 0x996633, 0x663300, new Item.Properties().stacksTo(16)));
+            () -> new ForgeSpawnEggItem(OTTER, 0x996633, 0x663300, new Item.Properties()));
 
 
-    public OttercraftCommon() {
+    public Ottercraft() {
         ModLoadingContext.get().registerConfig(net.minecraftforge.fml.config.ModConfig.Type.COMMON, Config.COMMON_SPEC);
         ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
         SOUNDS.register(FMLJavaModLoadingContext.get().getModEventBus());
@@ -58,35 +57,21 @@ public class OttercraftCommon {
     
     @SubscribeEvent
     public static void commonSetup(final FMLCommonSetupEvent event) {
-
+        SpawnPlacements.register(OTTER.get(), SpawnPlacements.Type.NO_RESTRICTIONS,
+                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Animal::checkMobSpawnRules);
     }
-    
     @SubscribeEvent
     public static void clientSetup(final FMLClientSetupEvent event) {
         EntityRenderers.register(OTTER.get(), OtterRenderer::new);
     }
 
-    // Replaced with BiomeModifiers
-    // https://forge.gemwire.uk/wiki/Biome_Modifiers
-    /*@SubscribeEvent
-    public void loadBiome(BiomeLoadingEvent event) {
-        if (event.getName() == null)
-            return; // Apparently this is a possibility!
-        if (Arrays.asList(Config.COMMON.otterSpawnBiomes.get().split(",")).contains(event.getName().toString()))
-            event.getSpawns().getSpawner(MobCategory.CREATURE)
-                    .add(new MobSpawnSettings.SpawnerData(OttercraftCommon.OTTER.get(),
-                            Config.COMMON.otterSpawnWeight.get(),
-                            Config.COMMON.otterSpawnMin.get(),
-                            Config.COMMON.otterSpawnMax.get()));
-    }*/
-    
+    // Add entity attributes
     @SubscribeEvent
     public static void createAttributes(final EntityAttributeCreationEvent event) {
         event.put(OTTER.get(), Otter.createAttributes().build());
-        SpawnPlacements.register(OTTER.get(), SpawnPlacements.Type.NO_RESTRICTIONS,
-                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Animal::checkMobSpawnRules);
     }
 
+    // Add the spawn eggs to creative mode tab
     @SubscribeEvent
     public static void createSpawnEggs(final CreativeModeTabEvent.BuildContents event) {
         if (event.getTab() == CreativeModeTabs.SPAWN_EGGS)
